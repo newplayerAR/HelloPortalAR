580f9f6e8431364bc85dc14089a45a292fee126d
newplayerAR <shens495@hotmail.com>
2019-03-03 19:24:09 -0500

    -first commit

---
+++ Unity-Technologies-unity-arkit-plugin-53b3d3b059f2/Assets/Scripts/snowParticlesController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class snowParticlesController : MonoBehaviour
+{
+    private ParticleSystem ps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        StartCoroutine(ProgressForward());
+    }
+
+    IEnumerator ProgressForward()
+    {
+        var main = ps.main;
+        main.simulationSpeed = 1000f;
+        ps.Play();
+        yield return new WaitForSeconds(0.1f);
+        main.simulationSpeed = 1f;
+    }
+}
